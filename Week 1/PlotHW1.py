ca7748087d4f9c0769590256a4a684d36c22c539
breyonna420 <pinkneybreyonna@yahoo.com>
2016-01-21 10:25:33 -0500

    Breyonna Pinkey HW1

--- Week 1/PlotHW1.py
+++ Week 1/PlotHW1.py
@@ -1,20 +1,16 @@
-#import numpy and matlibplots module
+import numpy as np
+from np import (sin, cos, pi )
+import matplotlib.pyplot as plt
 
-
-#generate array of x values from 0 to 6pi
-
-#calculate y values for sin wave
-
-#calculate y values for cosine wave
-
-#plot x and y values for both functions 
-
-#set dashed line for sine wave and solide line for cosine wave
-
-#add legend
-
-
-#leave comment here with your information
-#Name:
-#Date:
+x = np.linspace(0,6*np.pi,100)
+#y = np.linspace (0,6*np.pi,100)
+plt.plot (x, np.sin(x), '--', linewidth=2)
+plt.plot (x,np.cos(x), '-', linewidth=2)
+plt.ylabel("y")
+plt.xlabel("x")
+plt.legend ('sin(x)', 'cos(x)')
+plt.show
+#Breyonna Pinkney 
+#@02730553 
+# 1/21/16
 
